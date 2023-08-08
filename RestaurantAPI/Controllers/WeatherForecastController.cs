88faec93f76823c6971de79a4b4e79543e203362
Dawid Ruth <dawidolinp1@gmail.com>
2023-08-08 21:32:51 +0200

    create WeatherForecastService.cs and move the WeatherForecast service there

--- RestaurantAPI/Controllers/WeatherForecastController.cs
+++ RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -6,28 +6,32 @@ namespace RestaurantAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+
+
+        private readonly WeatherForecastService _service;
 
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            _service = new WeatherForecastService();
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+       
+       public IEnumerable<WeatherForecast> Get() 
+       {
+            var result = _service.Get();
+            return result;
+       }
+       
+       
+       
+       
+       
+       
+       
+       
     }
 }
